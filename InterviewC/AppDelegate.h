26e1c14f995570d8bc88d685499ebc9533b15470
Yerassyl Yerlanov <Yerassyl@MacBook-Pro-Yerassyl.local>
2017-04-02 20:00:16 +0600

    Initial Commit

---
+++ InterviewC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InterviewC
+//
+//  Created by Yerassyl Yerlanov on 02.04.17.
+//  Copyright © 2017 Yerassyl Yerlanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
